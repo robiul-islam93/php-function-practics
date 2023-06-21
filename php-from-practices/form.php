75d287199e7ca58625b171aaa12dfa44930bce8c
robiul-islam93 <robi.cnits@gmail.com>
2023-06-21 12:16:25 +0600

    first commit

---
+++ php-from-practices/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+input{
+  padding: 5px;
+  margin:5px;
+}
+</style>
+</head>
+<body>  
+
+<form action="form.php" method="post">
+Name: <input type="text" name = "fnme"><br>
+E-mail: <input type="email" name="email"><br>
+<input type="submit">
+</form>
+
+
+</body>
+</html>
